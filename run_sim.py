ebe261beb876bdd8681f401e4267df5a947ab977
Alexey Lavrov <alavrov@davisson.princeton.edu>
2014-11-30 18:01:05 -0500

    Added running of MATLAV analyzer to run_sim

--- run_sim.py
+++ run_sim.py
@@ -1,28 +1,46 @@
 #!/usr/bin/python
 
 import os, re
+from optparse import OptionParser
+
 
 def setEnv():
-	os.system('source /usr/licensed/synopsys/profile.20130806')
+    os.system('source /usr/licensed/synopsys/profile.20130806')
 
 def main():
-	setEnv()
+    setEnv()
 
-        os.system('rm -f wire_model_1.sp')
-	os.system('echo "exit;" >> wire_model_1.m')
-	os.system('matlab -nodisplay -nosplash -nodesktop -r "wire_model_1(100)"')
-	print 'Matlab finished generation of SPICE model'
+    # Parsing rutime arguments
+    parser = OptionParser()
+    parser.add_option("-N", dest="N",
+                  help="Number of chains", default=100)
 
-        fin = open('wire_model_1.sp', 'r')
-        cont = fin.read()
-        fin.close()
-        cont = re.sub(',', '', cont)
-	fout = open('wire_model_1.sp', 'w')
-        fout.write(cont)
-        fout.close()
-	print 'SPICE model has been processed for running with hspice'
+    (options, args) = parser.parse_args()
 
-        os.system('hspice wire_model_1.sp > hspice.log')	
+
+    print "number of stages: %d" % int(options.N)
+    print "Starting work"
+
+
+    os.system('rm -f wire_model_1.sp')
+    os.system('echo "exit;" >> wire_model_1.m')
+    os.system('matlab -nodisplay -nosplash -nodesktop -r "wire_model_1(%d)"' % int(options.N))
+    print 'Matlab finished generation of SPICE model'
+
+    fin = open('wire_model_1.sp', 'r')
+    cont = fin.read()
+    fin.close()
+    cont = re.sub(',', '', cont)
+    fout = open('wire_model_1.sp', 'w')
+    fout.write(cont)
+    fout.close()
+    print 'SPICE model has been processed for running with hspice'
+
+    os.system('hspice wire_model_1.sp > hspice.log')
+    print 'hspice finished work'
+
+    os.system('matlab -nodisplay -nosplash -nodesktop -r "hspice_output_analyzer(%d)"' % int(options.N))
+    print 'Matlab analyzer finished'
 
 if __name__ == '__main__':
-	main()
+    main()
