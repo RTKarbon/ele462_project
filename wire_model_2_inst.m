e638893262cee6e130cc438c8212e6f90bcd6c1e
Mohammad Shahrad <mshahrad@davisson.princeton.edu>
2014-12-28 19:18:55 -0500

    New Capacitance Model

--- wire_model_2_inst.m
+++ wire_model_2_inst.m
@@ -1,9 +1,9 @@
-function [elmore_delay] = wire_model_2_inst(N,tech,metal_layer,sigma_limit,fmin,fmax,Nf)
+function [comp_el_del,R,L,C] = wire_model_2_inst(N,tech,metal_layer,sigma_limit,sigma_step,fmin,fmax,Nf,draw_fig)
 %N   =   2500;            % Number of chain elements 
 edges = 15;
 
 Len_db = 50:50:2000;
-step = 0.01;
+step = sigma_step;
 %sigma_limit = 0.25;
 
 freq_db = linspace(fmin,fmax,Nf);             % 100MHz
@@ -33,8 +33,14 @@ switch tech
         Wid =   metal_wid(metal_layer);     % Width in micro-meters
         Hei =   metal_thick(metal_layer);   % Height
         Rs  =   Rs_db(metal_layer);         % Resistance per micro-meters sqaure (7ohm)
-        Cpps=   30e-18;         % Parallel plate capacitance per micro-meters square
-        Cfl =   40e-18;         % Fringing capacitance per micro-meter
+        %Cpps=   30e-18;         % Parallel plate capacitance per micro-meters square
+        %Cfl =   40e-18;         % Fringing capacitance per micro-meter
+        if metal_layer==1
+            h = metal_thick(1);    % Distance from substrate
+        else
+            h = metal_thick(1)+sum(metal_thick(1:metal_layer-1));    % Distance from substrate
+        end
+        Cw_o = e*( 1.15*(Wid/h) + 2.8*(Hei/h).^(0.222))*1e-6; % Total Cap per micro-meter
         Ll  =   0.38e-12+metal_layer*0.5e-15;   % Self Inductance per micro-meter
     case 45         % 45nm (Intel)
         % total 9 metal_layers
@@ -48,8 +54,14 @@ switch tech
         Wid =   metal_wid(metal_layer);       % Width in micro-meters
         Hei =   metal_thick(metal_layer);     % Height
         Rs  =   2*ro(meduim)/(Hei*1e-6);      % Resistance per micro-meters sqaure (7ohm)
-        Cpps=   30e-18;         % Parallel plate capacitance per micro-meters square
-        Cfl =   40e-18;         % Fringing capacitance per micro-meter
+        %Cpps=   30e-18;         % Parallel plate capacitance per micro-meters square
+        %Cfl =   40e-18;         % Fringing capacitance per micro-meter
+        if metal_layer==1
+            h = metal_thick(1);    % Distance from substrate
+        else
+            h = metal_thick(1)+sum(metal_thick(1:metal_layer-1));    % Distance from substrate
+        end
+        Cw_o = e*( 1.15*(Wid/h) + 2.8*(Hei/h).^(0.222))*1e-6; % Total Cap per micro-meter
         Ll  =   0.381e-12+metal_layer*0.67e-15; % Self Inductance per micro-meter
     case 32         % 32nm (Intel)
         % total 9 metal_layers
@@ -64,15 +76,21 @@ switch tech
         Wid =   metal_wid(metal_layer);       % Width in micro-meters
         Hei =   metal_thick(metal_layer);     % Height
         Rs  =   2*ro(meduim)/(Hei*1e-6);      % Resistance per micro-meters sqaure (7ohm)
-        Cpps=   30e-18;         % Parallel plate capacitance per micro-meters square
-        Cfl =   40e-18;         % Fringing capacitance per micro-meter
+        %Cpps=   30e-18;         % Parallel plate capacitance per micro-meters square
+        %Cfl =   40e-18;         % Fringing capacitance per micro-meter
+        if metal_layer==1
+            h = metal_thick(1);    % Distance from substrate
+        else
+            h = metal_thick(1)+sum(metal_thick(1:metal_layer-1));    % Distance from substrate
+        end
+        Cw_o = e*( 1.15*(Wid/h) + 2.8*(Hei/h).^(0.222))*1e-6; % Total Cap per micro-meter        
         Ll  =   0.381e-12+metal_layer*0.67e-15; % Self Inductance per micro-meter
 end
 
 Len =   20000;           % Length of wire in micro-meters
 Rw  =   Len*Rs/Wid;     % Wire resistance
-Cppw=   Len*Wid*Cpps;   % Wire parallel plate capacitance
-Cfw =   2*Len*Cfl;      % Wire fringing capacitance
+%Cppw=   Len*Wid*Cpps;   % Wire parallel plate capacitance
+%Cfw =   2*Len*Cfl;      % Wire fringing capacitance
 Lw  =   Len*Ll;
 C_ld=   0;              % Load capacitor at wire destination
 
@@ -133,13 +151,20 @@ for freq=1:length(freq_db)
             end
             tot_R(x,freq) = sum(R);
             % Stage capacitance calculation
-            Cpp =   ( (Cppw/N)*ones(1,N) ) .* (fac_w);              % Parallel plate apacitor per chain element
-            Cf  =   ( (Cfw/N)*ones(1,N) ) .* (fac_h);               % Parallel plate apacitor per chain element
-            C   =   Cpp + Cf;
+            %Cpp =  ( (Cppw/N)*ones(1,N) ) .*(fac_w);   % Parallel plate apacitor per chain element
+            % Better approximation by Eq. (4.2), p.141
+            %Cpp =  ( (Cppw/N)*ones(1,N) ) .*(fac_w*Wid-fac_h*Hei/2)./(Wid-Hei/2);   % Parallel plate apacitor per chain element
+            %Cf  =  ( (Cfw/N)*ones(1,N) ) .* (fac_h);               % Parallel plate apacitor per chain element
+            %C   =  Cpp + Cf;
+            C = zeros(1,N);
+            for i=1:N
+                C(i) = e*(Len/N)*( 1.15*(Wid*fac_w(i)/h) + 2.8*(Hei*fac_h(i)/h).^(0.222))*1e-6;
+            end
             tot_C(x,freq) = sum(C);
             % Stage inductance calculation
-            fac_l = (Cpp.*fac_w + Cf.*fac_h)./(Cpp+Cf);
-            L   =   Lw/N*ones(1,N)./fac_l;
+            
+            %fac_l = (Cpp.*fac_w + Cf.*fac_h)./(Cpp+Cf);
+            L   =  Lw/N*ones(1,N) .* (Cw_o*(Len/N)* ones(1,N)) ./C;
 
             comp_elmore_delay = 0;
 
@@ -161,44 +186,52 @@ for freq=1:length(freq_db)
     end
 
 end
-simp_elmore_delay = (N+1)*Rw*(Cppw + Cfw)/(2*N)
-%surf(round_w,round_h,comp_el_del)
-figure
-subplot(2,3,1)
-%hist(R,100)
-surf(freq_db,round_w,tot_R)
-ylabel('\sigma=\sigma_w=\sigma_h')
-xlabel('Frequency (Hz)')
-zlabel('R_t_o_t');
-subplot(2,3,2)
-surf(freq_db,round_w,comp_el_del)
-ylabel('\sigma=\sigma_w=\sigma_h')
-xlabel('Frequency (Hz)')
-zlabel('T_p');
-str = sprintf('Tech: %dnm  Metal Layer:%d  Length:%dmm',tech,metal_layer,Len/1000);
-title(str)
-subplot(2,3,3)
-contour(freq_db,round_w,comp_el_del,20)
-ylabel('\sigma=\sigma_w=\sigma_h')
-xlabel('Frequency (Hz)')
-subplot(2,3,4)
-%hist(C,100)
-freq_db_det=linspace(fmin,fmax,10*Nf);
-skin_depth_det = 1e6./sqrt(pi*freq_db_det.*permeability(meduim).*conductivity(meduim));
-semilogy(freq_db_det,2*skin_depth_det,'k');
-hold on
-grid on
-semilogy(freq_db_det,Wid*ones(1,length(freq_db_det)),'b');
-semilogy(freq_db_det,Hei*ones(1,length(freq_db_det)),'r');
-legend('2 \times Skin Depth','Nominal Width','Nominal Height');
-ylabel('(\mum)')
-xlabel('Frequency (Hz)')
-subplot(2,3,5)
-L_det = (2*pi).*freq_db_det*Lw;
-loglog(freq_db_det,L_det,'k');
-hold on
-grid on
-loglog(freq_db_det,Rw*ones(1,length(freq_db_det)),'b');
-legend('Nominal Wire wL','Nominal Wire R');
-ylabel('Impedance (ohm)')
-xlabel('Frequency (Hz)')
+
+if (draw_fig==1)
+    simp_elmore_delay = (N+1)*Rw*(Cw_o*Len)/(2*N)
+    %surf(round_w,round_h,comp_el_del)
+    figure
+    subplot(2,3,1)
+    %hist(R,100)
+    surf(freq_db,round_w,tot_R)
+    ylabel('\sigma=\sigma_w=\sigma_h')
+    xlabel('Frequency (Hz)')
+    zlabel('R_t_o_t');
+    subplot(2,3,2)
+    surf(freq_db,round_w,comp_el_del)
+    ylabel('\sigma=\sigma_w=\sigma_h')
+    xlabel('Frequency (Hz)')
+    zlabel('T_p');
+    str = sprintf('Tech: %dnm  Metal Layer:%d  Length:%dmm',tech,metal_layer,Len/1000);
+    title(str)
+    subplot(2,3,3)
+    contour(freq_db,round_w,comp_el_del,20)
+    ylabel('\sigma=\sigma_w=\sigma_h')
+    xlabel('Frequency (Hz)')
+    subplot(2,3,4)
+    %hist(C,100)
+    freq_db_det=linspace(fmin,fmax,10*Nf);
+    skin_depth_det = 1e6./sqrt(pi*freq_db_det.*permeability(meduim).*conductivity(meduim));
+    semilogy(freq_db_det,2*skin_depth_det,'k');
+    hold on
+    grid on
+    semilogy(freq_db_det,Wid*ones(1,length(freq_db_det)),'b');
+    semilogy(freq_db_det,Hei*ones(1,length(freq_db_det)),'r');
+    legend('2 \times Skin Depth','Nominal Width','Nominal Height');
+    ylabel('(\mum)')
+    xlabel('Frequency (Hz)')
+    subplot(2,3,5)
+    L_det = (2*pi).*freq_db_det*Lw;
+    loglog(freq_db_det,L_det,'k');
+    hold on
+    grid on
+    loglog(freq_db_det,Rw*ones(1,length(freq_db_det)),'b');
+    legend('Nominal Wire wL','Nominal Wire R');
+    ylabel('Impedance (ohm)')
+    xlabel('Frequency (Hz)')
+    subplot(2,3,6)
+    surf(freq_db,round_w,tot_C)
+    ylabel('\sigma=\sigma_w=\sigma_h')
+    xlabel('Frequency (Hz)')
+    zlabel('C_t_o_t');
+end
